a1c0dc3b405c5592533fe26eefaa3e868fb2f8b2
siaimes <34199488+siaimes@users.noreply.github.com>
2020-12-25 22:21:33 +0800

    在主节点部署成功！ Signed-off-by: siaimes <34199488+siaimes@users.noreply.github.com>

--- build.sh
+++
@@ -1,5 +0,0 @@
-./rmi.sh
-cd webportal_plugin
-sudo docker build -t siaimes_webportal_plugin:v1.2.0 .
-cd ../rest_server
-sudo docker build -t siaimes_rest_server:v1.2.0 .
--- rmi.sh
+++
@@ -1,2 +0,0 @@
-./rm.sh
-sudo docker rmi siaimes_webportal_plugin:v1.2.0 siaimes_rest_server:v1.2.0
